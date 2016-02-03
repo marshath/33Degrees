4d762d0a7d65c3b6346448f7fed401fd65c99399
Theodore Marshall <ted@tedm.net>
2016-02-02 18:19:39 -0800

    Add css formatting for contact, estimate, and confirmation pages.

--- header.php
+++ header.php
@@ -58,6 +58,7 @@
 		</script>
 		<?php } else {} ?>
 		
+		
 		<?php // ADDTHIS // ?>
 		<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-51b3d7b12af9a97b" async="async"></script>
 		
