4b38cd2c1624cdf221b8eef32c7d7c325d341ee9
Theodore Marshall <ted@tedm.net>
2025-10-22 11:54:08 -0700

    - add email marketing sidebar image, fix related projects sidebar query

--- sidebar.php
+++ sidebar.php
@@ -17,7 +17,6 @@
 						
 						<?php if ( is_singular('post') ) { // if blog post display related category posts
 							$categories = get_the_category($post->ID); // Get the current posts category
-							
 							// Check if categories exist
 							if ($categories) {
 								$category_ids = array(); // Get the IDs of the categories
@@ -63,23 +62,26 @@
 								wp_reset_postdata(); // reset post to main query to prevent conflicts
 							}
 						} else { // if project post display related classification posts
-							
-							$categories = get_the_terms( $post->ID, 'classification'); // Get the current posts category
-							
+								
+							$current_post_id = get_the_ID(); // Get the ID of the current post
+							$taxonomy_name = 'classifications'; // Custom taxonomy name
+							$terms = wp_get_post_terms( $current_post_id, $taxonomy_name, array('fields' => 'slugs'));
+														
 							// Check if categories exist
-							if ($categories) {
-								$category_ids = array(); // Get the IDs of the categories
-								foreach($categories as $category) {
-									$category_ids[] = $category->term_id;
-								}
+							if ( ! empty( $terms )) {
 								// Define the arguments for the related posts query
 								$args = array(
-									'category__in' => $category_ids,
-									'post__not_in' => array($post->ID),
+									'post_type' => 'project', // custom post type slug
 									'posts_per_page' => 2,
+									'post__not_in' => array( $current_post_id ), // exclude current post
+									'tax_query' => array(
+										'taxonomy' => $taxonomy_name,
+										'field' => 'slug',
+										'terms' => $terms,
+										'operator' => 'IN'
+									),
 									'orderby' => 'rand',
-									'order' => 'DESC',
-									'ignore_sticky_posts' => 1, // Exclude sticky posts
+									'order' => 'DESC'
 								);
 								
 								// Create a new WP Query instance
@@ -87,7 +89,7 @@
 								
 								// Check if any related posts were found
 								if ($related_posts_query) {
-									echo '<h3>Related <span class="screen-reader-text">' . $category -> name . '</span> Projects</h3>'; // Related articles title
+									echo '<h3>Related Projects</h3>'; // Related articles title
 									echo '<ul class="overlay-links updated">';
 									// The loop for related posts
 									while ($related_posts_query->have_posts()) {
